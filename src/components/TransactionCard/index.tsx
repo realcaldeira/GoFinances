17c5abbee5a557e7a5df3b34188a7b6c3f440146
Lucas Caldeira <mrrealcaldeira@gmail.com>
2021-08-24 14:16:19 -0300

    BUG: Listando as transações

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -18,7 +18,7 @@ interface Category {
 
 export interface TransactionCardProps {
     type: 'positive' | 'negative';
-    title: string;
+    name: string;
     amount: string;
     category: Category;
     date: string;
@@ -32,7 +32,7 @@ export function TransactionCard({ data } : Props){
     return(
         <Container>
             <Title>
-                {data.title}
+                {data.name}
             </Title>
             
             <Amount type={data.type}>
